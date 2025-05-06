5c114315f2fc8c063196a760cb1cfddcbfd08234
ankit satyal <ankitsatyal6@gmail.com>
2025-05-06 21:03:21 +0530

    lgtm

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ const App = () => {
     <div className='h-screen w-full grid  place-items-center  bg-slate-200'>
       <div className=' bg-white rounded-xl p-4 min-h-1/2  max-h-screen min-w-1/3 shadow-md shadow-gray-500 flex flex-col 
       justify-center items-center gap-4'>
+        {/* add to */}
         <Addtodo />
+        {/* previous todos  */}
         <Todos />
       </div>
     </div>
